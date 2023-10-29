d58c4613120d8445af66ac0d80e62a389c2ce02a
Hevander27 <45948489+Hevander27@users.noreply.github.com>
2023-10-29 17:24:46 -0400

    Add files via upload

---
+++ ios101-project6-tumblr-starter/ios101-project6-tumblr/PostCell.swift
@@ -0,0 +1,49 @@
+//
+//  PostCell.swift
+//  ios101-project6-tumblr
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var summaryLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+    
+    // MARK: - Get image data
+    private func getImageDataFrom(url: URL) {
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            // Handle Error
+            if let error = error {
+                print("DataTask error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                // Handle Empty Data
+                print("Empty Data")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data) {
+                    self.postImageView.image = image
+                }
+            }
+        }.resume()
+    }
+
+}
